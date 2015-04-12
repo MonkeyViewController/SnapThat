44210cfb97226b52276fa632b8dbeb15af837180
Josh <jadoctors@csupomona.edu>
2015-04-11 18:45:11 -0700

    Input validation and aesthetic changes for the signup activity.

--- app/src/main/java/com/monkeyviewcontroller/snapthat/SignupActivity.java
+++ app/src/main/java/com/monkeyviewcontroller/snapthat/SignupActivity.java
@@ -4,39 +4,118 @@ import android.app.Activity;
 import android.support.v4.app.DialogFragment;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class SignupActivity extends FragmentActivity implements View.OnClickListener {
+public class SignupActivity extends FragmentActivity {
 
     private Button btnSignupBirthday;
-    private static EditText etSignupBirthday;
+    private Button btnSignupSignup;
+
+    private EditText etSignupUsername;
+    private EditText etSignupEmail;
+    private EditText etSignupPassword;
+    private EditText etSignupBirthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
+        etSignupUsername = (EditText) findViewById(R.id.etSignupUsername);
+        etSignupEmail = (EditText) findViewById(R.id.etSignupEmail);
+        etSignupPassword = (EditText) findViewById(R.id.etSignupPassword);
         etSignupBirthday = (EditText) findViewById(R.id.etSignupBirthday);
+
         btnSignupBirthday = (Button)findViewById(R.id.btnSignupBirthday);
-        btnSignupBirthday.setOnClickListener(this);
+        btnSignupBirthday.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.d("MVC", "Clicked button to select birthday");
+                showDatePickerDialog(v);
+            }
+        });
+
+        btnSignupSignup = (Button) findViewById(R.id.btnSignupSignup);
+        btnSignupSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("MVC", "Clicked button to sign up");
+                attemptSignup();
+            }
+        });
+    }
+
+    public void attemptSignup()
+    {
+        etSignupUsername.setError(null);
+        etSignupEmail.setError(null);
+        etSignupPassword.setError(null);
+        etSignupBirthday.setError(null);
+
+        boolean cancel = false;
+        View focusView = null;
+
+        String username = etSignupUsername.getText().toString();
+        String email = etSignupEmail.getText().toString();
+        String birthday = etSignupBirthday.getText().toString();
+        String password = etSignupPassword.getText().toString();
+
+        if(TextUtils.isEmpty(birthday))
+        {
+            etSignupBirthday.setError(getString(R.string.error_field_required));
+            focusView = etSignupBirthday;
+            cancel = true;
+        }
+
+        if(TextUtils.isEmpty(password))
+        {
+            etSignupPassword.setError(getString(R.string.error_field_required));
+            focusView = etSignupPassword;
+            cancel = true;
+        }
+
+        if(!isPasswordValid(password))
+        {
+            etSignupPassword.setError(getString(R.string.error_invalid_password));
+            focusView = etSignupPassword;
+            cancel = true;
+        }
+
+        if(TextUtils.isEmpty(email))
+        {
+            etSignupEmail.setError(getString(R.string.error_field_required));
+            focusView = etSignupEmail;
+            cancel = true;
+        }
+
+        if(TextUtils.isEmpty(username))
+        {
+            etSignupUsername.setError(getString(R.string.error_field_required));
+            focusView = etSignupUsername;
+            cancel =  true;
+        }
+
+        if(cancel){
+            focusView.requestFocus();
+        }else {
+            Log.d("MVC", "Proceed to signup via parse.");
+            //show progress bar
+            //perform user signup
+            //navigate them back to new activity?
+        }
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
     }
 
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-        if (id == R.id.btnSignupBirthday) {
-            Log.d("MVC", "Clicked button select birthday");
-            showDatePickerDialog(v);
-        }
     }
 }
